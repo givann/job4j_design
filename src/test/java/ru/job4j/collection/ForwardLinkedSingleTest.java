bbc7d27b557ddb37ce8eb07f43e6be05b0330713
anton <zas@see.ru>
2020-12-15 00:41:51 -0500

    ReverseLL_1.0

--- src/test/java/ru/job4j/collection/ForwardLinkedSingleTest.java
+++ src/test/java/ru/job4j/collection/ForwardLinkedSingleTest.java
@@ -1,10 +1,22 @@
 package ru.job4j.collection;
 
 import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class ForwardLinkedSingleTest extends TestCase {
 
+    @Test
     public void testRevert() {
-
+        ForwardLinkedSingle<Integer> linked = new ForwardLinkedSingle<>();
+        linked.add(1);
+        linked.add(2);
+        linked.revert();
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(2));
     }
 }
