316ed22b67a0df941bd3b9f69c19d8eecc609af2
anton <zas@see.ru>
2021-01-26 00:04:51 -0500

    LogFilter_1.2

--- src/main/java/ru/job4j/IO/LogFilter.java
+++ src/main/java/ru/job4j/IO/LogFilter.java
@@ -7,12 +7,10 @@ import java.util.List;
 
 public class LogFilter {
     public static List<String> filter(String file) {
-        List<String> list = new ArrayList<>();
         List<String> newl = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line = reader.readLine();
             while (line != null) {
-                list.add(line);
                 if (line.contains("404")){
                     newl.add(line);
                 }
